5b7a67688faf85fbc960dd2c048f58e8816741c8
apurva bhavanasi <43738214+Apurva-bhavanasi123@users.noreply.github.com>
2020-05-02 18:45:36 +0530

    Delete memory.py

--- memory.py
+++
@@ -1,9 +0,0 @@
-from firebase import firebase
-
-
-firebase = firebase.FirebaseApplication('xxxxxxxxxxxxxxxx', None)
-data =  { 'url': 'John Doe',
-          
-          }
-result = firebase.post('/python-example-f6d0b/memes/',data)
-print(result)
